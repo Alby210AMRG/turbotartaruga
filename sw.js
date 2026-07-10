24120dbb29ce9ae9014d2318cf2a6a2c7a7b3812
Alby <alberto.rossi.0112@gmail.com>
2026-07-10 21:03:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TurboTartaruga Service Worker v202604262049
-const CACHE_NAME = 'turbotartaruga-202607101557';
+const CACHE_NAME = 'turbotartaruga-202607101901';
 
 // File da precacheare all'installazione (OBBLIGATORI per offline)
 const PRECACHE = [
